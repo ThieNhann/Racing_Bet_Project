970183cf9b05a7a5e60cc8838373f8eec804f5ab
dzqt1 <150364329+dzqt1@users.noreply.github.com>
2023-11-09 23:37:54 +0700

    Diem danh

--- Test Git _ Diem Danh/TestGit/diemdanh.cpp
+++ Test Git _ Diem Danh/TestGit/diemdanh.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
 	cout << "Vegito > Gogeta";
 	cout << "\nAe cout o duoi roi ghi ten minh, sau do add/commit/push len github nhe ^^";
-	cout <<"\nNg Thien Nhan".
+	cout << "\nNg Thien Nhan";
+	cout << "\nNhanThVo";
 }
