77df8283d93b0c20c53467ebebacf1fea193be33
ThieNhann <ngtnhan14@gmail.com>
2023-11-09 22:16:39 +0700

    NgThNhan diem danh

--- TestGit/diemdanh.cpp
+++ TestGit/diemdanh.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
 	cout << "Vegito > Gogeta";
-	cout << "\nAe cout o duoi roi ghi ten minh ra nhe ^^";
+	cout << "\nAe cout o duoi roi ghi ten minh, sau do add/commit/push len github nhe ^^";
+	cout <<"\nNg Thien Nhan".
 }
